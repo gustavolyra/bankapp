b2c50239c4239821e0f78a361b85f16dc6a3296f
Gustavo Lyra <gustavo.c.lyra@live.com>
2024-05-13 00:53:41 -0300

    fix, reset route when content-type application-json with length 0

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,26 @@
-import fastify from 'fastify'
+import fastify, { FastifyRequest } from 'fastify'
 import { ZodError } from 'zod'
 import { env } from './env'
 import { transactionsRoutes } from './http/transactions/routes'
 
-export const app = fastify()
+export const app = fastify();
+
+app.addContentTypeParser('application/json', { parseAs: 'string' }, function (req: FastifyRequest, body, done) {
+  try {
+    if (!body) body = '1'
+    var json = JSON.parse(body)
+    done(null, json)
+  } catch (err) {
+    err.statusCode = 400
+    done(err, undefined)
+  }
+})
+
+
 
 app.register(transactionsRoutes)
 
-app.setErrorHandler((error, _request, reply) => {
+app.setErrorHandler((error, request, reply) => {
   if (error instanceof ZodError) {
     return reply
       .status(404)
@@ -17,6 +30,5 @@ app.setErrorHandler((error, _request, reply) => {
   if (env.NODE_ENV !== 'production') {
     console.error(error)
   }
-
   return reply.status(500).send({ message: error.message })
 })
--- src/http/transactions/reset.ts
+++ src/http/transactions/reset.ts
@@ -3,9 +3,14 @@ import { FastifyReply, FastifyRequest } from "fastify";
 
 
 export async function reset(request: FastifyRequest, reply: FastifyReply) {
+  try {
+    const resetUseCase = makeResetUseCase();
+    await resetUseCase.execute();
 
-  const resetUseCase = makeResetUseCase()
-  await resetUseCase.execute()
-
-  return reply.status(200).send("OK")
+    return reply.status(200).send("OK");
+  } catch (error) {
+    console.error("Error in reset route:", error);
+    return reply.status(500).send({ message: error });
+  }
 }
+
